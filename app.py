3f40ecdcee86a3c0a747c3acc3223d50236a8653
Yash Naik <yashtnaik@gmail.com>
2025-04-06 20:25:53 +0530

    ggdfd

--- app.py
+++ app.py
@@ -50,4 +50,7 @@ def get_total_calories():
     return jsonify({'total_calories': total})
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000)
+
